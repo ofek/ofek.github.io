ceb56486ba614449150959379a99c286188e4c4b
Ofek Lev <ofekmeister@gmail.com>
2019-06-30 15:51:44 -0400

    see

--- scripts/access-theme.sh
+++ scripts/access-theme.sh
@@ -13,3 +13,4 @@ echo $'Host hydejack\n    HostName github.com\n    IdentitiesOnly yes\n    Ident
 echo "|1|L3rBLxsTRXA5C48+4DcV8hHvK4Y=|fFirAqNJJWk+xY7wkc703OcVH+w= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
 eval "$(ssh-agent -s)"
+git clone "git@hydejack:qwtel/hydejack-8-pro.git"
