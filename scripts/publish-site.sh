3a087cbedd15eb78585a3c6df35cc738be1aed48
Ofek Lev <ofekmeister@gmail.com>
2019-06-30 17:05:27 -0400

    add publish job

---
+++ scripts/publish-site.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euxo pipefail
+IFS=$'\n\t'
+
+echo "Publishing site..."
+
+rev=$(git rev-parse --short HEAD)
+
+cd _site
+
+git init
+git config user.name "Ofek Lev"
+git config user.email "ofekmeister@gmail.com"
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/ofek/ofek.github.io.git"
+git fetch upstream
+git reset upstream/master
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:master
+
+echo "Published to master branch."
