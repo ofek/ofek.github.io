c9c6cf8540a6fd508c395eeb94a1c56d55433fa1
Ofek Lev <ofekmeister@gmail.com>
2019-12-08 18:02:24 -0500

    hmm

--- scripts/remove-default-assets.sh
+++ scripts/remove-default-assets.sh
@@ -7,6 +7,12 @@ echo "Removing unnecessary assets..."
 
 theme=$(bundler show jekyll-theme-hydejack-pro)
 
+ls "$theme/assets"
+ls "$theme/assets/img"
+ls "$theme/assets/img/blog"
+ls "$theme/assets/img/docs"
+ls "$theme/assets/img/projects"
+
 rm "$theme/assets/img/blog/*"
 rm "$theme/assets/img/docs/*"
 rm "$theme/assets/img/projects/*"
